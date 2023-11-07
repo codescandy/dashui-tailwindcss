da1bb45759a74ef42cb752147d63b13f34a83d7f
anitaparmar26 <easeanita@gmail.com>
2023-11-07 15:19:42 +0530

    added docs with small content

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -84,4 +84,31 @@
 			new Offcanvas(offcanvasEl);
 		});
 	}
+
+	var dropzoneElements = document.querySelectorAll('#my-dropzone');
+
+	if (dropzoneElements.length) {
+		Dropzone.autoDiscover = false;
+
+		const myDropzone = new Dropzone('#my-dropzone', {
+			url: 'https://httpbin.org/post',
+			maxFilesize: 5,
+			acceptedFiles: 'image/*',
+			addRemoveLinks: true,
+			autoProcessQueue: true,
+		});
+
+		// Add event listeners
+		myDropzone.on('addedfile', function (file) {
+			console.log('File added: ' + file.name);
+		});
+
+		myDropzone.on('removedfile', function (file) {
+			console.log('File removed: ' + file.name);
+		});
+
+		myDropzone.on('success', function (file, response) {
+			console.log('File uploaded successfully:', response);
+		});
+	}
 })();
